d297a1875fddcf8a710cd41d1c72d4824ae594e4
Tess Ellenoir Duursma <tess.e.duursma@gmail.com>
2023-07-21 00:06:17 +0200

    Expand the test

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,6 @@
 import { restfulRepositoryFixture } from "./utils/testing";
 import { renderHook } from "@testing-library/react";
+import { sleep } from "./utils/time";
 
 test(
   "Test .use",
@@ -7,6 +8,11 @@ test(
     const { result, unmount } = renderHook(() => repository.use("posts/1"));
 
     expect(result.current.value).toBe(undefined);
+
+    await sleep(250);
+
+    expect(result.current.value).toBeDefined();
+    expect(result.current.value!.id).toBe(1);
 
     unmount();
   })
